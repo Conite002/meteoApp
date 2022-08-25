5acbca7fb107457bfebf04c173c45f6f25cb330d
conite002 <conite.gbodogbe@imsp-uac.org>
2022-08-25 23:05:27 +0200

    meteo

---
+++ scripts/main.js
@@ -0,0 +1,104 @@
+import tabJoursEnOrdre from './Utilitaire/gestionTemps.js';
+
+// console.log("DEPUIS MAIN JS:" + tabJoursEnOrdre);
+
+const CLEFAPI = 'af4a71fd9c5fb88c8c6e4413de36e9af';
+let resultatsAPI;
+
+const temps = document.querySelector('.temps');
+const temperature = document.querySelector('.temperature');
+const localisation = document.querySelector('.localisation');
+const name = document.querySelector('.name');
+const heure = document.querySelectorAll('.heure-nom-prevision');
+const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
+const joursDiv = document.querySelectorAll('.jour-prevision-nom');
+const tempJoursDiv = document.querySelectorAll('.jour-prevision-temp');
+const imgIcone = document.querySelector('.logo-meteo');
+const chargementContainer = document.querySelector('.overlay-icone-chargement');
+const blocLogo = document.querySelector('.bloc-logo');
+
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+
+        console.log(position);
+        let long = position.coords.longitude;
+        let lat = position.coords.latitude;
+        AppelAPI(long,lat);
+
+    }, () => {
+        alert(`Vous avez refusé la géolocalisation, l'application ne peur pas fonctionner, veuillez l'activer.!`)
+    })
+}
+
+function AppelAPI(long, lat) {
+
+    // fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${CLEFAPI}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${CLEFAPI}`)
+    .then((reponse) => {
+        return reponse.json();
+    })
+    .then((data) => {
+        // console.log(data);
+
+        resultatsAPI = data;
+        console.log(resultatsAPI)
+        // temps.innerText = resultatsAPI.current.weather[0].description;
+        temps.innerText = resultatsAPI.weather[0].description;
+        temperature.innerText = `${Math.trunc(resultatsAPI.main.temp)}°`
+        localisation.innerText = resultatsAPI.timezone;
+        name.innerText = resultatsAPI.name;
+
+
+        // les heures, par tranche de trois, avec leur temperature.
+
+        let heureActuelle = new Date().getHours();
+
+        for(let i = 0; i < heure.length; i++) {
+
+            let heureIncr = heureActuelle + i * 3;
+
+            if(heureIncr > 24) {
+                heure[i].innerText = `${heureIncr - 24} h`;
+            } else if(heureIncr === 24) {
+                heure[i].innerText = "00 h"
+            } else {
+                heure[i].innerText = `${heureIncr} h`;
+            }
+
+        }
+
+        // temp pour 3h
+        for(let j = 0; j < tempPourH.length; j++) {
+            tempPourH[j].innerText = `${Math.trunc(resultatsAPI.main.temp)}°`
+            // tempPourH[j].innerText = `${Math.trunc(resultatsAPI.hourly[j * 3].temp)}°`
+        }
+
+
+        // trois premieres lettres des jours 
+
+        for(let k = 0; k < tabJoursEnOrdre.length; k++) {
+            joursDiv[k].innerText = tabJoursEnOrdre[k].slice(0,3);
+        }
+
+
+        // Temp par jour
+        for(let m = 0; m < 7; m++){
+            tempJoursDiv[m].innerText = `${Math.trunc(resultatsAPI.main.temp)}°`
+            // tempJoursDiv[m].innerText = `${Math.trunc(resultatsAPI.daily[m + 1].temp.day)}°`
+        }
+
+        // Icone dynamique 
+         if(heureActuelle >= 6 && heureActuelle < 21) {
+             imgIcone.src = `ressources/jour/${resultatsAPI.weather[0].icon}.svg`
+            //  imgIcone.src = `ressources/jour/${resultatsAPI.current.weather[0].icon}.svg`
+         } else  {
+            imgIcone.src = `ressources/nuit/${resultatsAPI.weather[0].icon}.svg`
+         }
+
+
+         chargementContainer.classList.add('disparition');
+
+    })
+
+}
